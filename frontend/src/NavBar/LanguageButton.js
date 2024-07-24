7f170ea25bb6e4f87021b784fb4a69c4920602dd
Harun Onur <66617253+haro3535@users.noreply.github.com>
2024-07-24 23:50:23 +0300

    Footer update
    language de bag var

--- frontend/src/NavBar/LanguageButton.js
+++ frontend/src/NavBar/LanguageButton.js
@@ -11,7 +11,7 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid'
 export default function LanguageButton() {
   return (
     <>
-      <div className='relative hidden lg:block'>
+      <div className='relative '>
         <button className='md:inline-flex border-2 rounded-lg py-2 px-3 hidden hover:ring-[0.5px] hover:ring-gray-300' onClick={LanguageButtonOnClicked}>
           <svg className='mr-1' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--blue-second)">
               <path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q83 0 155.5 31.5t127 86q54.5 54.5 86 127T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Zm0-82q26-36 45-75t31-83H404q12 44 31 83t45 75Zm-104-16q-18-33-31.5-68.5T322-320H204q29 50 72.5 87t99.5 55Zm208 0q56-18 99.5-55t72.5-87H638q-9 38-22.5 73.5T584-178ZM170-400h136q-3-20-4.5-39.5T300-480q0-21 1.5-40.5T306-560H170q-5 20-7.5 39.5T160-480q0 21 2.5 40.5T170-400Zm216 0h188q3-20 4.5-39.5T580-480q0-21-1.5-40.5T574-560H386q-3 20-4.5 39.5T380-480q0 21 1.5 40.5T386-400Zm268 0h136q5-20 7.5-39.5T800-480q0-21-2.5-40.5T790-560H654q3 20 4.5 39.5T660-480q0 21-1.5 40.5T654-400Zm-16-240h118q-29-50-72.5-87T584-782q18 33 31.5 68.5T638-640Zm-234 0h152q-12-44-31-83t-45-75q-26 36-45 75t-31 83Zm-200 0h118q9-38 22.5-73.5T376-782q-56 18-99.5 55T204-640Z"/>
@@ -20,7 +20,7 @@ export default function LanguageButton() {
           <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
         </button>
         
-        <div id='language-menu' className='absolute hidden right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 cursor-pointer overflow-hidden' >
+        <div id='language-menu' className='absolute hidden right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 cursor-pointer overflow-hidden'>
           <div className='py-1 pl-3 hover:bg-gray-100'>
             <a className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
               Türkçe
@@ -28,7 +28,7 @@ export default function LanguageButton() {
           </div>
           <div className='py-1 pl-3 hover:bg-gray-100'>
             <a className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900'>
-              Englisch
+              English
             </a>
           </div>
         </div>
--- frontend/src/NavBar/NavBar.js
+++ frontend/src/NavBar/NavBar.js
@@ -21,7 +21,9 @@ export default function NavBar(){
                                 <li>İletişim</li>
                             </ul>
                         </div>
-                        <LanguageButton />
+                        <div className='hidden lg:block'>
+                            <LanguageButton />
+                        </div>
                         <svg className="block cursor-pointer hover:text-gray-500 lg:hidden w-6 h-6" onClick={ToggleButtonOnClicked} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
                         </svg>
@@ -35,7 +37,7 @@ export default function NavBar(){
                             <li className="py-2" onClick={CloseToggleMenu}>Projelerimiz</li>
                             <li className="py-2" onClick={CloseToggleMenu}>Referanslarımız</li>
                             <li className="py-2" onClick={CloseToggleMenu}>İletişim</li>
-                            <li className="py-2"><LanguageButton /></li>
+                            <li className="py-2"><LanguageButton className="z-10"/></li>
                         </ul>
                         
                         
