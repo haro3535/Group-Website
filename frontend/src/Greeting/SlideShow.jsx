18bc50b88b8e75c018f9d58e74f6d9bd1e24b92e
Ege Deniz Yaşar <91566475+egedenizz@users.noreply.github.com>
2024-07-25 02:37:21 +0300

    added auto-slide and changed css

--- frontend/src/Greeting/SlideShow.jsx
+++ frontend/src/Greeting/SlideShow.jsx
@@ -40,6 +40,12 @@ const SlideShow = () => {
     }
   }, [currentIndex, prevIndex]);
 
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+       nextSlide()
+    }, 5000);
+    return () => clearInterval(slideInterval);
+  }, [currentIndex, nextSlide]);
   return (
     <div className='carousel'>
       
