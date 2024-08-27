92aef945b8f07bcca50d57e32eb8619c9296c01e
Harun Onur <66617253+haro3535@users.noreply.github.com>
2024-08-27 22:46:01 +0300

    Update Footer.js
    Icon eklemeleri yapıldı.
    Iconlar Sayfadaki diğer iconlarla aynı yerden alınsa güzel olur

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -27,12 +27,13 @@ const Footer = () => {
     }
   };
   
+  
   return (
     <footer id="contact" className="bg-bcg-light-gray body-font flex flex-col items-center">
       <div className="w-3/4 grid lg:grid-cols-4 grid-cols-1 py-10 space-y-5 lg:space-y-0">
         <div className="w-full mb-5">
           <a href='/' className="text-4xl font-bold font-inter text-blue-second">
-            Bohesa
+            BOHESA
           </a>
           <p className="mt-2 text-sm text-light-gray">
           We prioritize customer satisfaction by developing websites
@@ -50,6 +51,15 @@ const Footer = () => {
                 <button className="text-light-gray hover:text-blue-third">Testimonials</button>
               </li>
               <li>
+                <button className="text-light-gray hover:text-blue-third">Privacy Policy</button>
+              </li>
+              <li>
+                <button className="text-light-gray hover:text-blue-third">Terms</button>
+              </li>
+              <li>
+                <button className="text-light-gray hover:text-blue-third">Cookie Policy</button>
+              </li>
+              <li>
                 {/*<a className="text-light-gray hover:text-blue-third">FAQ's</a> */}
               </li>
             </div>
@@ -63,11 +73,18 @@ const Footer = () => {
                 <button onClick={() => handleScrollToSection('services')} className="text-light-gray hover:text-blue-third">Our services</button>
               </li>
               <li>
-                <button onClick={() => handleScrollToSection('peoplePart')} className="text-light-gray hover:text-blue-third">People</button>
+                <button onClick={() => handleScrollToSection('solutions')} className="text-light-gray hover:text-blue-third">Business solutions</button>
               </li>
               <li>
-                <button className="text-light-gray hover:text-blue-third">Client</button>
+                <button onClick={() => handleScrollToSection('community')} className="text-light-gray hover:text-blue-third">Our Community</button>
               </li>
+              <li>
+                <button onClick={() => handleScrollToSection('about')} className="text-light-gray hover:text-blue-third">Who are we?</button>
+              </li>
+              <li>
+                <button onClick={() => handleScrollToSection('contact')} className="text-light-gray hover:text-blue-third">Contact Us</button>
+              </li>
+              
             </div>
           </div>
         </div>
@@ -75,17 +92,36 @@ const Footer = () => {
           <div className='lg:w-1/2 w-full'>
             <h2 className="title-font font-bold text-light-gray tracking-widest text-lg mb-3">Follow Us</h2>
             <div className="list-none mb-10">
-              <li>
-                <button className="text-light-gray hover:text-blue-third">LinkedIn</button>
+              <li className='flex space-x-3 items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#717171" fill="none">
+                    <path d="M7 10V17" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M11 13V17M11 13C11 11.3431 12.3431 10 14 10C15.6569 10 17 11.3431 17 13V17M11 13V10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M7.00801 7L6.99902 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
+                </svg>
+                <a href='/' className="text-light-gray hover:text-blue-third cursor-pointer">LinkedIn</a>
               </li>
-              <li>
-                <button className="text-light-gray hover:text-blue-third">Instagram</button>
+              <li className='flex space-x-3 items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#717171" fill="none">
+                    <path d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
+                    <path d="M16.5 12C16.5 14.4853 14.4853 16.5 12 16.5C9.51472 16.5 7.5 14.4853 7.5 12C7.5 9.51472 9.51472 7.5 12 7.5C14.4853 7.5 16.5 9.51472 16.5 12Z" stroke="currentColor" stroke-width="1.5" />
+                    <path d="M17.5078 6.5L17.4988 6.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <a href='/' className="text-light-gray hover:text-blue-third cursor-pointer">Instagram</a>
               </li>
-              <li>
-                <button className="text-light-gray hover:text-blue-third">X</button>
+              <li className='flex space-x-3 items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#717171" fill="none">
+                    <path d="M3 21L10.5484 13.4516M21 3L13.4516 10.5484M13.4516 10.5484L8 3H3L10.5484 13.4516M13.4516 10.5484L21 21H16L10.5484 13.4516" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <a href='/' className="text-light-gray hover:text-blue-third cursor-pointer">X</a>
               </li>
-              <li>
-                <button className="text-light-gray hover:text-blue-third">Medium</button>
+              <li className='flex space-x-3 items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#717171" fill="none">
+                    <circle cx="6.5" cy="12" r="4.5" stroke="currentColor" stroke-width="1.5" />
+                    <ellipse cx="15.5" cy="12" rx="2.5" ry="4.5" stroke="currentColor" stroke-width="1.5" />
+                    <ellipse cx="21" cy="12" rx="1" ry="4.5" stroke="currentColor" stroke-width="1.5" />
+                </svg>
+                <a href='/' className="text-light-gray hover:text-blue-third cursor-pointer">Medium</a>
               </li>
             </div>
           </div>
