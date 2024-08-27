ba5ff07ddce0ed724d65a928d795222c29cfbd3d
Harun Onur <66617253+haro3535@users.noreply.github.com>
2024-08-27 23:22:17 +0300

    Update About.js
    Boyut düzenlemeleri yapıldı

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function About() {
     return (
-        <div id="about" className="flex justify-center items-center py-10 bg-white">
-            <div className="flex flex-col md:flex-row items-center w-full max-w-screen-xl px-4">
+        <section id="about" className='flex justify-center'>
+            <div className="w-3/4 flex flex-col md:flex-row space-y-6 justify-center items-center py-10 bg-white">
                 {/* Image */}
-                <div className="md:w-1/2 mb-6 md:mb-0">
+                <div className="flex justify-center md:w-1/2 mb-6 md:mb-0">
                     <img 
                         src="/people.png" 
                         alt="Team" 
@@ -13,8 +13,8 @@ export default function About() {
                     />
                 </div>
                 {/* Text */}
-                <div className="md:w-1/2 text-center md:text-left px-4">
-                    <div className="mb-6">
+                <div className="md:w-1/2 md:text-left px-4">
+                    <div className="mb-6 space-y-3 md:space-y-0">
                         <h2 className="text-xl font-semibold inter-font" style={{ color: 'var(--custom-gray)' }}>Who Are We?</h2>
                         <h1 className="text-m font-normal inter-font" style={{ color: 'var(--light-gray)' }}>
                             We are a team of passionate developers dedicated to providing top-notch software solutions. We envision a world where every business can leverage technology to achieve their goals.
@@ -22,6 +22,6 @@ export default function About() {
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
